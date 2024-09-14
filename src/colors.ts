24f752edc7e8ad556e0a48a421ac2cfccb961334
Louise Lehman <luhlahlehman@gmail.com>
2024-09-14 15:44:43 -0600

    feat: removing old files

--- src/colors.ts
+++
@@ -1,6 +0,0 @@
-export const COLORS = {
-    'headerTextColor': 'white',
-    'mainTextColor': 'black',
-    'peachy': 'rgb(255, 164, 94, 0.4)',
-    'lightBlue': '#5CAAEF'
-};
--- src/components/IconLink/IconLink.js
+++
@@ -1,9 +0,0 @@
-const IconLink = ({title, href, className}) => {
-    return (
-        <a href={href} className={`fa ${className} solo`}>
-            {title}
-        </a>
-    );
-}
-
-export default IconLink;
